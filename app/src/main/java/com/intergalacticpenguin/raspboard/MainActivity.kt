894a84104e7de6b511fe1f6ea0206a64c6f385cb
Adam Svoboda <eidamsvoboda@gmail.com>
2017-10-06 01:39:21 +0200

    get news article list display top article display debug text

--- app/src/main/java/com/intergalacticpenguin/raspboard/MainActivity.kt
+++ app/src/main/java/com/intergalacticpenguin/raspboard/MainActivity.kt
@@ -2,9 +2,18 @@ package com.intergalacticpenguin.raspboard
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.card_article.*
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.concurrent.TimeUnit
+
 
 /**
  * Skeleton of an Android Things activity.
@@ -28,13 +37,61 @@ import java.util.*
  */
 class MainActivity : Activity() {
 
+    val newsService by lazy {
+        NewsService.create()
+    }
+    var disposable: Disposable? = null
+    val dateFormat = SimpleDateFormat("dd. MM. yyyy hh:mm:ss")
+    val debugTextList = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val dateFormat = SimpleDateFormat("dd. MM. yyyy hh:mm:ss")
-        val dateString = dateFormat.format(Date())
+        // display resolution
+        val metrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(metrics)
+        debugTextList.add(metrics.widthPixels.toString()+"x"+metrics.heightPixels.toString())
 
-        text_view.setText(dateString)
+        setupDebugText()
+        getNews()
+    }
+
+    private fun getNews(): Disposable {
+        return newsService.getNews("bbc-news", "top", API_KEY)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { result -> showCard(result.articles.get(0).title, result.articles.get(0).description) },
+                        { error -> Log.e("Error", error.message) }
+                )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        disposable?.dispose()
+    }
+
+    fun setupDebugText() {
+        Observable
+                .interval(1L, TimeUnit.SECONDS)
+                .timeInterval()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ updateDebugText(debugTextList) })
+    }
+
+    fun updateDebugText(debugTextList: List<String>) {
+        var debugText = StringBuilder()
+        debugText.append(dateFormat.format(Date()))
+        for (string: String in debugTextList) {
+            debugText.append(" "+string)
+        }
+        debug_text.setText(debugText.toString())
+    }
+
+    fun showCard(title: String, content: String) {
+        card_title.setText(title)
+        card_content.setText(content)
     }
 }
---
+++ app/src/main/java/com/intergalacticpenguin/raspboard/NewsService.kt
@@ -0,0 +1,42 @@
+package com.intergalacticpenguin.raspboard
+
+import io.reactivex.Observable
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+var API_KEY:String = "9a1faf50080147ca9e63b80c12972c50"
+
+interface NewsService {
+
+    @GET("v1/articles")
+    fun getNews(@Query("source") source: String,
+             @Query("sortBy") sortBy: String,
+             @Query("apiKey") apiKey: String): Observable<ResultArticle>
+
+    /**
+     * Companion object to create the NewsService
+     */
+
+    companion object Factory {
+
+        fun create(): NewsService {
+            val okhttpClient = OkHttpClient.Builder()
+            val loggingInterceptor = HttpLoggingInterceptor()
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BASIC)
+
+            val retrofit = Retrofit.Builder()
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl("https://newsapi.org/")
+                    .client(okhttpClient.addInterceptor(loggingInterceptor).build())
+                    .build()
+
+            return retrofit.create(NewsService::class.java);
+        }
+    }
+}
